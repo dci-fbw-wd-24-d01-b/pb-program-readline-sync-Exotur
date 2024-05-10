13521db77a43f97b111980ea6177c2037f985738
kevin <k.fenrich@outlook.de>
2024-05-10 10:08:25 +0200

    neu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import readlineSync from "readline-sync";
+
+let firstName = readlineSync.question ("waht is your first name");
+let born = readlineSync.question ("What is your Birthyear?");
+let cityBorn = readlineSync.question ("in which City you where Born?");
+let city = readlineSync.question ("which city you live now");
+let year = readlineSync.question ("the actuall year")
+
+console.log(`hallo, mein name ist ${firstName}, ich wurde vor ${year - born} in ${cityBorn} geboren, jetzt lebe ich in ${city}`);
